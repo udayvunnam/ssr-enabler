f43b3d17e2efe500505d2e9d46d5d32559fa89ed
Uday Vunnam <uday4vunnam@gmail.com>
2019-08-27 22:01:34 +0530

    providing axios instance on server side

--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "../client/reducers";
+import axios from "axios";
 
-export default () => {
-  const store = createStore(reducers, {}, applyMiddleware(thunk));
+export default req => {
+  const axiosInstance = axios.create({
+    baseURL: "http://react-ssr-api.herokuapp.com",
+    cookie: req.get("cookie") || ""
+  });
+
+  const store = createStore(reducers, {}, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
   return store;
 };
