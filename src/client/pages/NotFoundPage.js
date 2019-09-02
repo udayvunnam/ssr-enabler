bb35c11d57cf49750d1ec3b87fe2d13d952afe30
Uday Vunnam <uday4vunnam@gmail.com>
2019-09-02 08:00:10 +0530

    sending 404 Not Found status to the browser

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const NotFoundPage = () => (<h1>Oops, the page is not available</h1>);
+const NotFoundPage = ({ staticContext = {} }) => {
+  staticContext.notFound = true;
+  return <h1>Oops, the page is not available</h1>;
+};
 
 export default {
   component: NotFoundPage
--- src/index.js
+++ src/index.js
@@ -26,16 +26,14 @@ app.get('*', (req, res) => {
 
   const promises = matchRoutes(Routes, req.path)
     .map(({ route }) => (route.loadData ? route.loadData(store) : null));
-    // .map((promise) => {
-    //   if (promise) {
-    //     return new Promise((resolve) => {
-    //       promise.then(resolve).catch(resolve);
-    //     });
-    //   }
-    // });
 
   Promise.all(promises).then(() => {
-    res.send(renderer(req, store));
+    const context = {};
+    const content = renderer(req, store, context);
+    if (context.notFound) {
+      res.status(404);
+    }
+    res.send(content);
   });
 });
 
